54e3c5ec4bf2b784319e2f611a7e91ce18030e56
ryananjasmara <ryananjasmara1997@gmail.com>
2020-09-26 15:58:27 +0700

    remove Api /users, /pencari and /pemilik

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -11,64 +11,6 @@ class Api extends RestController {
 		$this->load->model('Model_Pencari');
 	}
 
-	public function users_get()
-	{
-        // Users from a data store e.g. database
-		$users = [
-			['id' => 0, 'name' => 'John', 'email' => 'john@example.com'],
-			['id' => 1, 'name' => 'Jim', 'email' => 'jim@example.com'],
-		];
-
-		$id = $this->get( 'id' );
-
-		if ( $id === null )
-		{
-            // Check if the users data store contains users
-			if ( $users )
-			{
-                // Set the response and exit
-				$this->response( $users, 200 );
-			}
-			else
-			{
-                // Set the response and exit
-				$this->response( [
-					'status' => false,
-					'message' => 'No users were found'
-				], 404 );
-			}
-		}
-		else
-		{
-			if ( array_key_exists( $id, $users ) )
-			{
-				$this->response( $users[$id], 200 );
-			}
-			else
-			{
-				$this->response( [
-					'status' => false,
-					'message' => 'No such user found'
-				], 404 );
-			}
-		}
-	}
-
-	public function pencari_get() {
-		$data = $this->Model_Pemilik->ambil_data();
-		$this->response( [
-			'data' => $data
-		], 200 );
-	}
-
-	public function pencari_post() {
-		$id_pemilik = $this->post('id');
-		$data = $this->Model_Pencari->ambil_data();
-		$this->response( [
-			'data' => $id_pemilik
-		], 200 );
-	}
-
 	public function login_post() {
 		$login_type = $this->post('login_type');
 		$nomor_handphone = $this->post('nomor_handphone');
